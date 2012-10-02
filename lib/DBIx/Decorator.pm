c461c10487ac8273bac5aeb50b3ac649655d1dc2
Takuya Tsuchida <tsucchi@cpan.org>
2012-10-02 09:53:44 +0900

    initial commit

---
+++ lib/DBIx/Decorator.pm
@@ -0,0 +1,32 @@
+package DBIx::Decorator;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+DBIx::Decorator -
+
+=head1 SYNOPSIS
+
+  use DBIx::Decorator;
+
+=head1 DESCRIPTION
+
+DBIx::Decorator is
+
+=head1 AUTHOR
+
+Takuya Tsuchida E<lt>tsucchi {at} cpan.orgE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
